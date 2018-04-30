92b659e3c1f9b91ac0189518c4b5d268c49dbea7
Juan Fox <foxj0875@gmail.com>
2018-04-30 12:57:44 -0400

    installed express-jwt and set up server.js to connect to mongodb and use authroutes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const expressJWT = require('express-jwt');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/alessistantdb");
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
+
+//Verify token on every request
+//app.use(expressJWT({secret:'my secret'}));
+
+//Routes
+app.use(require('./routes/authRoutes'));
 
 app.get('/test', function(req, res) {
   res.send('<h1>test</h1>');
