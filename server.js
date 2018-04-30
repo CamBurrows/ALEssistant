0bf11b72595afbbe833670bd3e9c2186917fa250
Juan Fox <foxj0875@gmail.com>
2018-04-30 15:24:51 -0400

    server.js correction

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const expressJWT = require('express-JWT');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-var db = require("./models");
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/alessistantdb");
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
 
-mongoose.connect("mongodb://localhost/alessistant");
 
-db.User.create({ name: "Ernest" })
-  .then(function(dbUser) {
-    console.log(dbUser);
-  })
-  .catch(function(err) {
-    console.log(err.message);
-  });
+//Verify token on every request
+//app.use(expressJWT({secret:'my secret'}));
 
-// app.get('/test', function(req, res) {
-//   res.send('<h1>test</h1>');
-// });
+//Routes
+app.use(require('./routes/authRoutes'));
+
+app.get('/test', function(req, res) {
+  res.send('<h1>test</h1>');
+});
 
 // Start the API server
 app.listen(PORT, function() {
