34ed1dc5bcc0ce6ba19257201a22f6bdd1a23a70
Brandt <brandt.strait@gmail.com>
2018-04-29 22:59:07 -0400

    file structure and added images

---
+++ src/components/login/LoginNav.js
@@ -0,0 +1,15 @@
+import React from "React";
+import "LoginNav.css";
+
+const LoginNav = props => (
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4"></h1>
+    <p class="lead"></p>
+  </div>
+</div>
+
+);
+
+export default LoginNav;
