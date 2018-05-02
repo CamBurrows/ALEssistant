d4893bf016da496a6df13f49816d57793ba25c68
timhayes00 <timhayes00@gmail.com>
2018-05-02 10:42:37 -0400

    another small tweak, moving on to changing the controllers next

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,12 +21,12 @@ router.get('/users/:id', function(req, res) {
 //Needs testing and validation
 //PostMan is throwing an error for this:
 //"localhost:3001/api/users/?email=tim@foo.com&userName=tim&password=foo"
-router.post('/users/:id', function(req, res) {
-  console.log("req.params = " + req.params)
-  db.User.create({email: req.params.email, userName: req.params.userName, password: req.params.password})
-  .then(function(dbUser){res.json(dbUser)})
-  .catch(error)
-});
+// router.post('/users/:id', function(req, res) {
+//   console.log("req.params = " + req.params)
+//   db.User.create({email: req.params.email, userName: req.params.userName, password: req.params.password})
+//   .then(function(dbUser){res.json(dbUser)})
+//   .catch(error)
+// });
 
 //Seems to work, but please validate
 router.delete('/users/:id', function(req, res) {
