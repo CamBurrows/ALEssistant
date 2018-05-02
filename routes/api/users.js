52707cdda18b2d75c8a85e0d7a5779f175987c19
timhayes00 <timhayes00@gmail.com>
2018-05-01 23:24:36 -0400

    small tweaks to models, made a bunch of routes and verified most of them with PostMan

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require("../../models/");
 
+//PostMan says it works
 router.get('/users', function(req, res) {
   db.User.find({})
   .then(
@@ -8,11 +9,32 @@ router.get('/users', function(req, res) {
   )
 });
 
+//PostMan says it works
 router.get('/users/:id', function(req, res) {
   db.User.find({_id:req.params.id})
   .then(
     dbUser => res.json(dbUser)
   )
 });
+
+
+//Needs testing and validation
+//PostMan is throwing an error for this:
+//"localhost:3001/api/users/?email=tim@foo.com&userName=tim&password=foo"
+router.post('/users/:id', function(req, res) {
+  console.log("req.params = " + req.params)
+  db.User.create({email: req.params.email, userName: req.params.userName, password: req.params.password})
+  .then(function(dbUser){res.json(dbUser)})
+  .catch(error)
+});
+
+//Seems to work, but please validate
+router.delete('/users/:id', function(req, res) {
+  db.User.remove({_id:req.params.id})
+  .then(
+    dbUser => res.json(dbUser)
+  )
+});
+
 
 module.exports = router;
