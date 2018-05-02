a9c70f82fd231d2d07398fe5653b446664092170
Juan Fox <foxj0875@gmail.com>
2018-05-02 12:57:18 -0400

    all user api and auth functions moved to usersController.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,44 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models');
-var jwt = require('jsonwebtoken');
+const usersController = require('../controllers/usersController.js');
 
-router.post('/signup', function(req, res) {
-  console.log(req.body);
-  db.User.create(req.body)
-    .then(function(user) {
-      console.log(user);
-      res.json(user);
-    })
-    .catch(function(err) {
-      console.log(err);
-      res.json({message: 'did not create'});
-    });
-  
-  
-});
+
+router.post('/signup', usersController.create);
 
  
 
-router.post('/signin', function(req, res) {
-  if(!req.body.username){
-    res.status(400).send('username required');
-  }
-  if(!req.body.username){
-    res.status(400).send('username required');
-  }
-  db.User.findOne({username: req.body.username})
-    .then(function(user) {
-      //compare passwords
-      var token = jwt.sign({userId: user.id, username: user.username}, 'my secret');
-      res.status(200).json({
-        token: token,
-        user: user
-      });
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+router.post('/signin', usersController.start);
 
 module.exports = router;
