1ede3560c4a77dc0f61f57dc2d655d9430c98f6d
Cameron Burrows <cburrows@Camerons-MacBook-Pro.local>
2018-05-01 23:43:11 -0400

    lots of work done in recipe panel component

--- client/src/components/InvPageTitle/InvPageTitle.js
+++ client/src/components/InvPageTitle/InvPageTitle.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './InvPageTitle.css';
 
 const InvPageTitle = (props) => (
-    <div className="row">
-        <div className="col">
-            <div className="container-fluid">
-                <h1>Inventory</h1>
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col">
+                <div className="container-fluid">
+                    <h1>Inventory</h1>
+                </div>
             </div>
         </div>
     </div>
--- client/src/components/RecipePageTitle/RecipePageTitle.js
+++ client/src/components/RecipePageTitle/RecipePageTitle.js
@@ -3,16 +3,18 @@ import './RecipePageTitle.css';
 import AddRecipeButton from '../AddRecipeButton';
 
 const RecipePageTitle = (props) => (
-    <div className="row title-row">
-        <div className="col-sm-3">
-            <div className="container-fluid">
-                <h1>Recipes</h1>
+    <div className="container-fluid">
+        <div className="row title-row">
+            <div className="col-sm-3">
+                <div className="container-fluid">
+                    <h1>Recipes</h1>
+                </div>
             </div>
-        </div>
-        <div className="col-sm-7">
-        </div>
-        <div className="col-sm-2">
-        <AddRecipeButton />
+            <div className="col-sm-7">
+            </div>
+            <div className="col-sm-2">
+            <AddRecipeButton />
+            </div>
         </div>
     </div>
 )
