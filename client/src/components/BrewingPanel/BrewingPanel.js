4b4bbaabd7ca4daef4501a9f5feaea433087fee2
Cameron Burrows <cburrows@wifi_stu-10-38-71-223.uncc.edu>
2018-05-07 13:49:01 -0400

    functional brewing page

--- client/src/components/BrewingPanel/BrewingPanel.js
+++ client/src/components/BrewingPanel/BrewingPanel.js
@@ -10,23 +10,23 @@ const BrewingPanel = (props) => (
                     <div className="row">
                         <div className="col-sm-2">
                             <div className="recipeName">
-                                <h2 className="recipe-header">Recipe</h2>
+                                <h2 className="recipe-header">{props.recipeName}</h2>
                             </div>
                         </div>
                         <div className="col-sm-8">
                         </div>
                         <div className="col-sm-2">
-                            <div className="unitCost">
+                            {/* <div className="unitCost">
                                 <h2 className="unitCost">Unit Cost: $120</h2>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-2">
                             <div className="card">
                                 <div className="card-body">
-                                    <h4 className="brew">Brew</h4>
-                                    <h6>Heavenly Hopster</h6>
+                                    <h4 className="brew">Brew Started at:</h4>
+                                    <h6>{props.timestamp}</h6>
                                 </div>
                             </div>
                         </div>
@@ -34,7 +34,9 @@ const BrewingPanel = (props) => (
                             <div className="card">
                                 <div className="card-body">
                                     <h4 className="Fermentation">Fermentation</h4>
-                                    <h6> In progress</h6>
+                                    <h6>{props.fermentationComplete ? (
+                                        <p>Completed</p>
+                                    ): (<p>Still in fermentation</p> )}</h6>
                                 </div>
                             </div>
                         </div>
@@ -42,27 +44,34 @@ const BrewingPanel = (props) => (
                             <div className="card">
                                 <div className="card-body">
                                     <h4 className="packaging">Packaging</h4>
-                                    <h6> Awaiting Brew Completion</h6>
+                                    <h6>{props.packaged ? (
+                                        <p>Ready to Drink!</p>
+                                    ): (<p>Still in fermenter</p> )}</h6>
                                 </div>
                             </div>
                         </div>
                        
-                        <div className="col-sm-3">
+                        {/* <div className="col-sm-3">
                             <div className="card">
                                 <div className="card-body">
                                     <h4 className="current-state">Current State</h4>
-                                    <h6>In progress</h6>
+                                    <h6></h6>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="col-sm-1">
-                            <button type="button" className="btn right">
+                            <button onClick={props.onClick} type="button" className="btn right">
                                 <img className="nav-icon-right" src="/images/right-arrow.png" alt="" />
                             </button>
                         </div>
+                        <div className="col-sm-2">
+                            <button onClick={props.deleteOnClick} type="button" className="btn">
+                                <img className="nav-icon-right" src="/images/x.png" alt="" />
+                            </button>
+                        </div>
                     </div>
-                    <br></br>
-                    <div className="row">
+                    {/* <br></br> */}
+                    {/* <div className="row">
                         <div className="col-md-12">
                             <div className="card">
                                 <div className="card-body">
@@ -73,7 +82,7 @@ const BrewingPanel = (props) => (
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- client/src/pages/Brewing.js
+++ client/src/pages/Brewing.js
@@ -12,49 +12,99 @@ class Brewing extends React.Component {
         super(props);
 
         this.state = {
-        user: null,
-        allRecipes: [],
-        
+            user: null,
+            allRecipes: [],
+
         }
     }
 
     componentWillMount = () => {
-        this.setState({user: JSON.parse(localStorage.getItem('user'))})
+        this.setState({ user: JSON.parse(localStorage.getItem('user')) })
     }
-    
+
     componentDidMount = () => {
         // this.setState({user: JSON.parse(localStorage.getItem('user'))})
         console.log(this.state.user.user._id)
         this.loadRecipes(this.state.user.user._id)
-  
+
     }
-    
-    loadRecipes = () => {
-        API.getRecipes()
-          .then(res =>
-            this.setState({ allRecipes: res.data })
-          )
-          .catch(err => console.log(err));
+
+    loadRecipes = (userId) => {
+        API.getRecipes(userId)
+            .then(res =>
+                this.setState({ allRecipes: res.data })
+            )
+            .catch(err => console.log(err));
     };
 
-    // handleBatchProgress = id => {
-    //     event.preventDefault();
-        
-    //     if (this.state.brewNewBatch) {
+    handleBatchProgress = (id, index) => {
+        API.findRecipe(id)
+            .then(res => {
 
-    //     }
-    // };
+                if (!res.data[0].sessions[index].fermentationComplete) {
+
+                    const updatedSession = res.data[0]
+                    updatedSession.sessions[index].fermentationComplete = true
+
+                    API.updateRecipe(id, updatedSession)
+                        .then(res => this.loadRecipes(this.state.user.user._id))
+
+                }
+                else if (!res.data[0].sessions[index].packaged) {
+
+                    const updatedSession2 = res.data[0]
+                    updatedSession2.sessions[index].packaged = true
+
+                    API.updateRecipe(id, updatedSession2)
+                        .then(res => this.loadRecipes(this.state.user.user._id))
+                }
+            })
 
 
-    render(){
+    };
+
+    handleBatchRemove = (id, index) => {
+        API.findRecipe(id)
+        .then(res => {
+            const deleteSession = res.data[0]
+            deleteSession.sessions.splice(index,1)
+
+            API.updateRecipe(id, deleteSession)
+            .then(res => this.loadRecipes(this.state.user.user._id))
+        })
+    }
+
+
+    render() {
         return (
             <Wrapper>
-                <Navbar userName={this.state.user.user.userName} logout={this.props.logout}/>
-                <BrewPageTitle/>
-                <BrewingPanel/>
-            
+                <Navbar userName={this.state.user.user.userName} logout={this.props.logout} />
+                <BrewPageTitle />
+                {this.state.allRecipes.length ? (
+                    this.state.allRecipes.map(recipe => (
+
+                        recipe.sessions.map((session, index) => (
+                            <BrewingPanel
+                                timestamp={session.dateBrewed}
+                                onClick={() => this.handleBatchProgress(recipe._id, index)}
+                                deleteOnClick = {() => this.handleBatchRemove(recipe._id, index)}
+                                brewed={session.brewed}
+                                fermentationComplete={session.fermentationComplete}
+                                packaged={session.packaged}
+                                recipeName={recipe.name}
+                            />
+                        ))
+                        
+                    ))
+                ) : (
+
+                        <h4>No sessions yet. Head to recipes tab to start brewing!</h4>
+
+                    )}
+
             </Wrapper>
         )
+
     }
 }
 
