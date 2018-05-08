7d6f4b3d91e91ecf2995c685cc09c5943bc6f7ad
Cameron Burrows <cburrows@wifi_stu-10-38-74-130.uncc.edu>
2018-05-08 11:20:50 -0400

    updated build

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve up static assets
+app.use(express.static("client/build"));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
