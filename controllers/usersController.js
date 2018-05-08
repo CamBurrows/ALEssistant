588efe52e269277c8d77d378ed3bff1c6a486354
Juan Fox <foxj0875@gmail.com>
2018-05-08 15:42:01 -0400

    user controller cleanup

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,17 +2,23 @@ const db = require('../models');
 var jwt = require('jsonwebtoken');
 
 module.exports = {
+
+  //Sends all user data to '/api/users'
   findAll: function(req, res) {
     db.User.find({})
     .then(dbUser => res.json(dbUser))
     .catch(err => console.log(err));
   },
+
+  //Sends the user data of the user with the user id supplied to '/api/users/:id 
   findOne: function(req, res) {
     db.User.find({_id:req.params.id})
     .populate("recipes")
     .populate("ingredientsInventory")
     .then(dbUser => res.json(dbUser))
   },
+
+  //Creates a user with a token on sign up
   create: function(req, res) {
     db.User.create(req.body)
       .then(user => {
@@ -26,11 +32,15 @@ module.exports = {
         console.log(err);
         res.json({message: 'did not create'});
       });
-  }, 
+  },
+  
+  //Deletes a user and all user data from the database
   remove: function(req, res) {
     db.User.remove({_id:req.params.id})
     .then(dbUser => res.json(dbUser))
   },
+
+  //Logs a user in, and creates a token
   start: function(req, res) {
     if(!req.body.email){
       res.status(400).send('email required');
